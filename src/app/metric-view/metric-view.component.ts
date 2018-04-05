c3e6a853b3560c565e940662063fae444f90b1fa
Austin Pennyfeather <pennyfea@ualberta.ca>
2018-04-04 19:39:23 -0700

    navigational buttons complete

--- src/app/metric-view/metric-view.component.ts
+++ src/app/metric-view/metric-view.component.ts
@@ -5,8 +5,7 @@ import { ImageService } from '../metric-details/shared/image.service';
 import { LibraryService } from '../library.service';
 import { Library } from '../library';
 import { GraphService } from '../graph.service';
-import { map } from 'rxjs/operators';
-
+import { map,mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-metric-view',
@@ -22,41 +21,51 @@ visibleImages: any[] = [];
 
 activeId = 0;
 
-get activeImage() {
- return this.image[this.activeId];
-}
 
   constructor(private imageService: ImageService, private libraryService:LibraryService, private graphService:GraphService, private location: Location, private route: ActivatedRoute, private router: Router) {
     this.visibleImages = this.imageService.getImages();
    }
 
+  // ngOnInit() {
+  //   this.getLibrary();
+  //
+  //   this.image = this.imageService.getImage(
+  //    +this.route.snapshot.params['id'])
+  //
+  //   this.graph = this.graphService.getGraph(
+  //     +this.route.snapshot.params['id'])
+  //
+  //     this.route.paramMap.pipe(map((params) => params.get('id') || 0),map(n => Number(n))).subscribe(id => this.activeId = id);
+  //
+  // }
+
+
   ngOnInit() {
-    this.getLibrary();
+    const id$ = this.route.paramMap.pipe(map((params) => params.get('id') || 0), map(n => Number(n)));
+    id$.subscribe(id => {
+      this.activeId = id;
+      this.graph = this.graphService.getGraph(id);
+      this.image = this.imageService.getImage(id);
+    });
 
-    this.image = this.imageService.getImage(
-     +this.route.snapshot.params['id'])
-
-    this.graph = this.graphService.getGraph(
-      +this.route.snapshot.params['id'])
-
-      this.route.paramMap.pipe(map((params) => params.get('id') || 0),map(n => Number(n))).subscribe(id => this.activeId = id);
-
-  }
-
-  getLibrary(): void{
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.libraryService.getLibrary(id)
+    id$.pipe(mergeMap(id => this.libraryService.getLibrary(id)))
       .subscribe(library => this.library = library);
   }
+  //
+  // getLibrary(): void{
+  //   const id = +this.route.snapshot.paramMap.get('id');
+  //   this.libraryService.getLibrary(id)
+  //     .subscribe(library => this.library = library);
+  // }
 
   next() {
-    const next = this.activeId + 1 >= this.image.length - 1 ? this.graph.length -1 : this.activeId + 1;
-    this.router.navigate('/image/' + [next])
+    const next = this.activeId + 1 >= this.image.length - 1 ? this.graph.length - 1 : this.activeId + 1;
+    this.router.navigate(['/image/' + next]);
   }
 
   prev() {
     const prev = this.activeId - 1 < 0 ? 0 : this.activeId - 1;
-    this.router.navigate('/image/' + [prev])
+    this.router.navigate(['/image/' + prev]);
   }
 
 }
